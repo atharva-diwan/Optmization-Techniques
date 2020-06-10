8344b7d27029868e1f78f5bc6645a15af07c9eb2
atharva-diwan <64583671+atharva-diwan@users.noreply.github.com>
2020-06-10 12:52:29 +0530

    Add files via upload

---
+++ Batch Gradient Descent.py
@@ -0,0 +1,14 @@
+"""Batch Gradient Descent"""
+X = data_input
+Y = labels
+parameters = initialize_parameters(layers_dims)
+for i in range(0, num_iterations):
+    # Forward propagation
+    a, caches = forward_propagation(X, parameters)
+    # Compute cost.
+    cost += compute_cost(a, Y)
+    # Backward propagation.
+    grads = backward_propagation(a, caches, parameters)
+    # Update parameters.
+    parameters = update_parameters(parameters, grads)
+        
